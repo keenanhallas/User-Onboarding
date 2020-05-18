7d165d1c875a6602c1407fa1ab23590ea023973e
Keenan <keenanhallas@gmail.com>
2020-05-18 11:40:10 -0500

    two more tests passing

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -6,4 +6,12 @@ describe("Tests for the Lambda Cypress Testing project", () => {
     it("Types a name into the name field & checks the value", () => {
         cy.get('#name').type("Keenan Hallas").should("have.value", "Keenan Hallas");
     });
+
+    it("Types an email into the email field", () => {
+        cy.get('#email').type("keenanhallas@gmail.com");
+    });
+
+    it("Types a password into the password field", () => {
+        cy.get('#password').type("password123");
+    });
 });
